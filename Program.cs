ff1806c24c440cf0947fb8b7f9dc808694439a94
Jane Khalameizer <Jane.Khalameizer@scorpion.co>
2020-08-03 12:45:09 -0700

    pushing changes

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumber = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int j = randomNumber.Next(1, 100);
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
